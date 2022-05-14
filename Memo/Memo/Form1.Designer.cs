6654223db9ceed3e602952cfba3c46fdc66a9df2
방수현 <bangs5282@gmail.com>
2022-05-14 21:24:10 +0900

    bangs5282

---
+++ Memo/Memo/Form1.Designer.cs
@@ -0,0 +1,170 @@
+﻿
+namespace Memo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.materialFlatButton1 = new MaterialSkin.Controls.MaterialFlatButton();
+            this.richTextBox = new System.Windows.Forms.RichTextBox();
+            this.treeView = new System.Windows.Forms.TreeView();
+            this.materialFlatButton2 = new MaterialSkin.Controls.MaterialFlatButton();
+            this.materialFlatButton3 = new MaterialSkin.Controls.MaterialFlatButton();
+            this.LogBox = new System.Windows.Forms.RichTextBox();
+            this.materialFlatButton4 = new MaterialSkin.Controls.MaterialFlatButton();
+            this.SuspendLayout();
+            // 
+            // materialFlatButton1
+            // 
+            this.materialFlatButton1.AutoSize = true;
+            this.materialFlatButton1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.materialFlatButton1.Depth = 0;
+            this.materialFlatButton1.Font = new System.Drawing.Font("Roboto", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.materialFlatButton1.Location = new System.Drawing.Point(14, 421);
+            this.materialFlatButton1.Margin = new System.Windows.Forms.Padding(5, 10, 5, 10);
+            this.materialFlatButton1.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialFlatButton1.Name = "materialFlatButton1";
+            this.materialFlatButton1.Primary = false;
+            this.materialFlatButton1.Size = new System.Drawing.Size(70, 36);
+            this.materialFlatButton1.TabIndex = 0;
+            this.materialFlatButton1.Text = "Refresh";
+            this.materialFlatButton1.UseVisualStyleBackColor = true;
+            this.materialFlatButton1.Click += new System.EventHandler(this.materialFlatButton1_Click);
+            // 
+            // richTextBox
+            // 
+            this.richTextBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(240)))), ((int)(((byte)(240)))));
+            this.richTextBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox.Location = new System.Drawing.Point(228, 76);
+            this.richTextBox.Name = "richTextBox";
+            this.richTextBox.Size = new System.Drawing.Size(500, 332);
+            this.richTextBox.TabIndex = 1;
+            this.richTextBox.Text = "";
+            // 
+            // treeView
+            // 
+            this.treeView.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(240)))), ((int)(((byte)(240)))));
+            this.treeView.Location = new System.Drawing.Point(12, 76);
+            this.treeView.Name = "treeView";
+            this.treeView.Size = new System.Drawing.Size(210, 332);
+            this.treeView.TabIndex = 2;
+            this.treeView.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView_AfterSelect);
+            // 
+            // materialFlatButton2
+            // 
+            this.materialFlatButton2.AutoSize = true;
+            this.materialFlatButton2.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.materialFlatButton2.Depth = 0;
+            this.materialFlatButton2.Font = new System.Drawing.Font("Roboto", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.materialFlatButton2.Location = new System.Drawing.Point(94, 421);
+            this.materialFlatButton2.Margin = new System.Windows.Forms.Padding(5, 10, 5, 10);
+            this.materialFlatButton2.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialFlatButton2.Name = "materialFlatButton2";
+            this.materialFlatButton2.Primary = false;
+            this.materialFlatButton2.Size = new System.Drawing.Size(87, 36);
+            this.materialFlatButton2.TabIndex = 3;
+            this.materialFlatButton2.Text = "Download";
+            this.materialFlatButton2.UseVisualStyleBackColor = true;
+            this.materialFlatButton2.Click += new System.EventHandler(this.materialFlatButton2_Click);
+            // 
+            // materialFlatButton3
+            // 
+            this.materialFlatButton3.AutoSize = true;
+            this.materialFlatButton3.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.materialFlatButton3.Depth = 0;
+            this.materialFlatButton3.Font = new System.Drawing.Font("Roboto", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.materialFlatButton3.Location = new System.Drawing.Point(228, 421);
+            this.materialFlatButton3.Margin = new System.Windows.Forms.Padding(5, 10, 5, 10);
+            this.materialFlatButton3.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialFlatButton3.Name = "materialFlatButton3";
+            this.materialFlatButton3.Primary = false;
+            this.materialFlatButton3.Size = new System.Drawing.Size(46, 36);
+            this.materialFlatButton3.TabIndex = 4;
+            this.materialFlatButton3.Text = "Save";
+            this.materialFlatButton3.UseVisualStyleBackColor = true;
+            // 
+            // LogBox
+            // 
+            this.LogBox.BackColor = System.Drawing.Color.DimGray;
+            this.LogBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.LogBox.ForeColor = System.Drawing.SystemColors.Window;
+            this.LogBox.Location = new System.Drawing.Point(12, 470);
+            this.LogBox.Name = "LogBox";
+            this.LogBox.Size = new System.Drawing.Size(716, 247);
+            this.LogBox.TabIndex = 5;
+            this.LogBox.Text = "";
+            // 
+            // materialFlatButton4
+            // 
+            this.materialFlatButton4.AutoSize = true;
+            this.materialFlatButton4.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.materialFlatButton4.Depth = 0;
+            this.materialFlatButton4.Font = new System.Drawing.Font("Roboto", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.materialFlatButton4.Location = new System.Drawing.Point(645, 421);
+            this.materialFlatButton4.Margin = new System.Windows.Forms.Padding(5, 10, 5, 10);
+            this.materialFlatButton4.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialFlatButton4.Name = "materialFlatButton4";
+            this.materialFlatButton4.Primary = false;
+            this.materialFlatButton4.Size = new System.Drawing.Size(81, 36);
+            this.materialFlatButton4.TabIndex = 6;
+            this.materialFlatButton4.Text = "Show Log";
+            this.materialFlatButton4.UseVisualStyleBackColor = true;
+            this.materialFlatButton4.Click += new System.EventHandler(this.materialFlatButton4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(740, 468);
+            this.Controls.Add(this.materialFlatButton4);
+            this.Controls.Add(this.LogBox);
+            this.Controls.Add(this.materialFlatButton3);
+            this.Controls.Add(this.materialFlatButton2);
+            this.Controls.Add(this.treeView);
+            this.Controls.Add(this.richTextBox);
+            this.Controls.Add(this.materialFlatButton1);
+            this.Font = new System.Drawing.Font("Roboto", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
+            this.Name = "Form1";
+            this.Text = "Manager";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private MaterialSkin.Controls.MaterialFlatButton materialFlatButton1;
+        private System.Windows.Forms.RichTextBox richTextBox;
+        private System.Windows.Forms.TreeView treeView;
+        private MaterialSkin.Controls.MaterialFlatButton materialFlatButton2;
+        private MaterialSkin.Controls.MaterialFlatButton materialFlatButton3;
+        private System.Windows.Forms.RichTextBox LogBox;
+        private MaterialSkin.Controls.MaterialFlatButton materialFlatButton4;
+    }
+}
+
---
+++ Memo/Memo/Form1.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using MaterialSkin.Controls;
+using MaterialSkin;
+using FTP;
+using System.IO;
+
+namespace Memo
+{
+    public partial class Form1 : MaterialForm
+    {
+        ftpUtil FTP;
+        public Form1()
+        {
+            InitializeComponent();
+
+            FTP = new ftpUtil("", "", "*", "");
+            /*
+            var materialSkinManager = MaterialSkinManager.Instance;
+            materialSkinManager.AddFormToManage(this);
+            materialSkinManager.Theme = MaterialSkinManager.Themes.LIGHT;
+            materialSkinManager.ColorScheme = new ColorScheme(Primary.Green800, Primary.Green900, Primary.Green500, Accent.Orange400, TextShade.WHITE);
+            */
+        }
+
+        private bool GetNodes(ref TreeNode svrNode, string name)
+        {
+            string[] fileList = FTP.GetFileList(name);
+            List<string> infoList = new List<string>();
+            infoList = FTP.GetFilesDetailList(name);
+
+            for (int i = 0; i < infoList.Count; i++)
+            {
+                string filePath = fileList[i].Split('/')[1];
+
+                if (infoList[i][0] == 'd')
+                {
+                    TreeNode temp = new TreeNode(filePath, 0, 0);
+                    GetNodes(ref temp, name + "/" + filePath);
+                    svrNode.Nodes.Add(temp);
+                }
+                else
+                {
+                    svrNode.Nodes.Add(name + "/" + filePath, filePath, 1, 1);
+                }
+            }
+
+            return true;
+        }
+
+        private void materialFlatButton1_Click(object sender, EventArgs e) // 새로고침 버튼
+        {
+            treeView.Nodes.Clear();
+
+            // 이미지 다운로드
+            FTP.Download(@Application.StartupPath + "/Images/folder.png", "memo/Image/folder.png");
+            FTP.Download(@Application.StartupPath + "/Images/note.png", "memo/Image/note.png");
+            Log("[Download] " + Application.StartupPath + "/Images/folder.png");
+            Log("[Download] " + Application.StartupPath + "/Images/note.png");
+
+            // 이미지 할당
+            ImageList imgList = new ImageList();
+            imgList.Images.Add(Bitmap.FromFile(Application.StartupPath + "/Images/folder.png"));
+            imgList.Images.Add(Bitmap.FromFile(@Application.StartupPath + "/Images/note.png"));
+            treeView.ImageList = imgList;
+
+            // 서버 파일 가져오기
+            TreeNode svrNode = new TreeNode("서버", 0, 0);
+            bool temp = GetNodes(ref svrNode, "memo");
+
+            if (temp)
+            {
+                Log("파일 목록 불러오기 성공");
+            }
+            else
+            {
+                Log("파일 목록 불러오기 실패");
+
+            }
+
+
+            treeView.Nodes.Add(svrNode);
+            treeView.ExpandAll();
+        }
+        
+        private void downLoad()
+        {
+            if (treeView.SelectedNode == null)
+            {
+                MessageBox.Show("다운로드할 파일을 선택해주세요");
+            }
+            else
+            {
+                var selectedNode = treeView.SelectedNode;
+
+                if (selectedNode.Text == "")
+                {
+                    MessageBox.Show("폴더 다운로드 기능은 아직 준비중입니다");
+                }
+                else
+                {
+                    string localPath = @Application.StartupPath + "/Downloads/" + selectedNode.Text;
+
+                    FTP.Download(localPath, selectedNode.Name); Log("[Download] " + localPath);
+                    richTextBox.Clear();
+
+                    if (selectedNode.Text.Contains(".png") || selectedNode.Text.Contains(".jpg")) // 이미지파일
+                    {
+                        Bitmap oBitmap = null;
+
+                        oBitmap = (Bitmap)Bitmap.FromFile(Application.StartupPath + "/Downloads/" + selectedNode.Text);
+
+                        Clipboard.Clear();
+
+                        Clipboard.SetImage(oBitmap);
+
+                        richTextBox.Paste();
+                    }
+                    else // 일반파일
+                    {
+                        richTextBox.AppendText(File.ReadAllText(localPath));
+                    }
+                    //FTP.Upload("asdasd", "asd");
+                }
+            }
+        }
+
+        private void materialFlatButton2_Click(object sender, EventArgs e) // 다운로드 버튼
+        {
+            downLoad();
+        }
+
+        private void treeView_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            downLoad();
+        }
+
+        private void Log(string str)
+        {
+            LogBox.AppendText("\n" + str);
+        }
+
+        private void materialFlatButton4_Click(object sender, EventArgs e)
+        {
+            if(materialFlatButton4.Text == "Hide Log")
+            {
+                materialFlatButton4.Text = "Show Log";
+                this.Size = new Size(740, 468);
+            }
+            else
+            {
+                materialFlatButton4.Text = "Hide Log";
+                this.Size = new Size(740, 729);
+            }
+        }
+    }
+}
